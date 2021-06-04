6055a6cdbfebed2deecb62705176a419a8e04774
Quentin Gallois <guaire94.perso@gmail.com>
2021-06-04 17:30:30 +0400

    PO feedback

--- Usiqee/Utils/Ressources/Fonts/Fonts.swift
+++ Usiqee/Utils/Ressources/Fonts/Fonts.swift
@@ -28,7 +28,7 @@ enum Fonts: String {
         }
         enum Form {
             static var textField: UIFont {
-                Fonts.SofiaProBold.withSize(with: 17)
+                Fonts.SofiaProRegular.withSize(with: 17)
             }
             static var fieldDescription: UIFont {
                 Fonts.SofiaProBold.withSize(with: 17)
